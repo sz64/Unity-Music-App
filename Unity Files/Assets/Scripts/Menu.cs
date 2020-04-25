8e7354ecaef74494d818d4c921aa5f7f3d75fc73
Steven <sz64@bu.edu>
2020-04-25 15:22:39 -0400

    created menu

---
+++ Unity Files/Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Drum ()
+    {
+        SceneManager.LoadScene("Drum");
+    }
+    public void Piano ()
+    {
+        SceneManager.LoadScene("Piano");
+    }
+    public void Quit ()
+    {
+        Application.Quit();
+    }
+    public void Back ()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
---
+++ Unity Files/Assets/Scripts/Piano.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piano : MonoBehaviour
+{
+    public AudioSource C;
+    public AudioSource D;
+    public AudioSource E;
+    public AudioSource F;
+    public AudioSource G;
+    public AudioSource A;
+    public AudioSource B;
+    public AudioSource C1;
+    public AudioSource D1;
+    public AudioSource E1;
+    public AudioSource Cs;
+    public AudioSource Ds;
+    public AudioSource Fs;
+    public AudioSource Gs;
+    public AudioSource As;
+    public AudioSource C1s;
+    public AudioSource D1s;
+    
+    public void Play_C ()
+    {
+        C.Play();
+    }
+    public void Play_D ()
+    {
+        D.Play();
+    }
+    public void Play_E ()
+    {
+        E.Play();
+    }
+    public void Play_F ()
+    {
+        F.Play();
+    }
+    public void Play_G ()
+    {
+        G.Play();
+    }
+    public void Play_A ()
+    {
+        A.Play();
+    }
+    public void Play_B ()
+    {
+        B.Play();
+    }
+    public void Play_C1 ()
+    {
+        C1.Play();
+    }
+    public void Play_D1 ()
+    {
+        D1.Play();
+    }
+    public void Play_E1 ()
+    {
+        E1.Play();
+    }
+    public void Play_Cs ()
+    {
+        Cs.Play();
+    }
+    public void Play_Ds ()
+    {
+        Ds.Play();
+    }
+    public void Play_Fs ()
+    {
+        Fs.Play();
+    }
+    public void Play_Gs ()
+    {
+        Gs.Play();
+    }
+    public void Play_As ()
+    {
+        As.Play();
+    }
+        public void Play_C1s ()
+    {
+        C1s.Play();
+    }
+    public void Play_D1s ()
+    {
+        D1s.Play();
+    }
+}
